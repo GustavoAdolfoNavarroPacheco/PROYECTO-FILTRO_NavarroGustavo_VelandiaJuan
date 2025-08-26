8652391d5eb015a2524fdbf4a786aa2646f251c1
VelandiaJuan <ryu.mitsuky.87@gmail.com>
2025-08-25 19:47:14 -0500

    feat :zap: add new data and logic in all pages

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -6,17 +6,28 @@ window.addEventListener('DOMContentLoaded', () => {
     return;
   }
 
-  document.querySelector('.username').innerHTML = user.fullName.split(' ').slice(0, 2).join('<br>');
+
+  document.querySelector('.username').innerHTML = user.fullName
+    .split(' ')
+    .slice(0, 2)
+    .join('<br>');
+
+
   document.querySelector('.profilePic').style.backgroundImage = `url('${user.avatar}')`;
   document.querySelector('.dropdown-avatar').style.backgroundImage = `url('${user.avatar}')`;
 
+
+  document.getElementById('fullName').textContent = user.fullName;
+
   const profileButton = document.getElementById('profileButton');
   const dropdownMenu = document.getElementById('dropdownMenu');
+
 
   profileButton.addEventListener('click', (e) => {
     e.stopPropagation();
     dropdownMenu.classList.toggle('show');
   });
+
 
   window.addEventListener('click', () => {
     dropdownMenu.classList.remove('show');
