f047a56aec5a2a66a4b4f0ad7b61b3b7683cf4ec
VelandiaJuan <ryu.mitsuky.87@gmail.com>
2025-08-26 16:25:41 -0500

    feat :zap: add new data logic api in dashboard

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -6,30 +6,38 @@ window.addEventListener('DOMContentLoaded', () => {
     return;
   }
 
-
   document.querySelector('.username').innerHTML = user.fullName
     .split(' ')
     .slice(0, 2)
     .join('<br>');
 
-
   document.querySelector('.profilePic').style.backgroundImage = `url('${user.avatar}')`;
   document.querySelector('.dropdown-avatar').style.backgroundImage = `url('${user.avatar}')`;
-
 
   document.getElementById('fullName').textContent = user.fullName;
 
   const profileButton = document.getElementById('profileButton');
   const dropdownMenu = document.getElementById('dropdownMenu');
 
-
   profileButton.addEventListener('click', (e) => {
     e.stopPropagation();
     dropdownMenu.classList.toggle('show');
   });
 
-
   window.addEventListener('click', () => {
     dropdownMenu.classList.remove('show');
   });
+
+  if (user.role === 'student' && Array.isArray(user.progress) && user.progress.length > 0) {
+    const progressData = user.progress[0].total;
+    const overallProgressElement = document.querySelector('.things1 .coursePercentage');
+    if (overallProgressElement) {
+      overallProgressElement.textContent = progressData + '%';
+
+      console.log(user);
+console.log(user.progress);
+console.log(progressData);
+
+    }
+  }
 });
